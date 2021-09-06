f97681ce1bb6c3455d29665f6f765961ca40f60d
ikkiwar <ikkiofbodom@gmail.com>
2021-09-05 21:06:19 -0600

    hide url from node database

--- index.js
+++ index.js
@@ -1,74 +1,72 @@
-var Express = require("express")
-var  bodyParser = require("body-parser")
+var DB_CONECTION = require("./urlDb");
 
+var Express = require("express");
+var bodyParser = require("body-parser");
 
-var app = Express()
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
+var app = Express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 var MongoClient = require("mongodb").MongoClient;
-var CONNECTION_STRING = "mongodb+srv://ikki:ikkiFenix07@cluster0.zsvfj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+var CONNECTION_STRING = DB_CONECTION;
 
-var DATABASE = "testdb"
+var DATABASE = "testdb";
 var db;
 
-
-app.listen(49146, ()=>{
-    MongoClient.connect(CONNECTION_STRING,{useNewUrlParser: true},(error,client) => {
-        database = client.db(DATABASE)
-        console.log("Mongo DB connection successful")
-    })
+app.listen(49146, () => {
+  MongoClient.connect(
+    CONNECTION_STRING,
+    { useNewUrlParser: true },
+    (error, client) => {
+      database = client.db(DATABASE);
+      console.log("Mongo DB connection successful");
+    }
+  );
 });
 
 app.get("/", (request, response) => {
-    response.send("Hello, World!")
-})
+  response.send("Hello, World!");
+});
 
 app.get("/api/department", (request, response) => {
+  database
+    .collection("Departament")
+    .find({})
+    .toArray((error, result) => {
+      if (error) {
+        console.log(error);
+      }
 
-    database.collection("Departament").find({}).toArray((error, result) => {
-if (error) {
-    console.log(error)
-}
-
-response.send(result)
-
-    })
-
-})
+      response.send(result);
+    });
+});
 
 app.post("/api/department", (request, response) => {
+  database.collection("Departament").count({}, function (error, numOfDocs) {
+    if (error) {
+      console.log(error);
+    }
 
-   database.collection("Departament").count({}, function(error,numOfDocs){
-        if (error) {
-            console.log(error)
-        }
+    database.collection("Departament").insertOne({
+      DepartmentId: numOfDocs + 1,
+      DepartmentName: request.body["DepartmentName"],
+    });
 
-        database.collection("Departament").insertOne({
-            DepartmentId: numOfDocs+1,
-            DepartmentName: request.body["DepartmentName"]
-        });
-
-        response.send("Added Successfuly")
-   } )
-
-})
-
+    response.send("Added Successfuly");
+  });
+});
 
 app.put("/api/department", (request, response) => {
+  database.collection("Departament").updateOne(
+    {
+      DepartmentId: request.body["DepartmentId"],
+    },
+    {
+      $set: {
+        DepartmentName: request.body["DepartmentName"],
+      },
+    }
+  );
 
-         database.collection("Departament").updateOne({
-             DepartmentId: request.body["DepartmentId"]
-         },
-         { $set:
-            {
-                "DepartmentName": request.body["DepartmentName"]
-            }
-
-         }
-         );
- 
-         response.send("Added Successfuly")
-  
- 
- })
+  response.send("Added Successfuly");
+});
